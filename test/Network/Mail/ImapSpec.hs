8402ce7778f1fcfcb85ca9c086dd2d2080247351
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-01-06 23:23:43 +0100

    Add first draft of DSL for Imap

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -3,11 +3,20 @@ module Network.Mail.ImapSpec (main, spec) where
 import Test.Hspec
 import Network.Mail.Imap
 
+runStubTest :: Imap a -> Expectation
+runStubTest _ = True `shouldBe` True
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "useless" $ do
-    it "empty test" $ do
-      True `shouldBe` True
+  describe "DSL usecases" $ do
+    it "Print INBOX" $ runStubTest $ do
+      messages <- fetchAll
+      return [    show (getUID m)
+        ++ " " ++ show (getDate m)
+        ++ " " ++ show (getSender m)
+        ++ " " ++ show (getSubject m)
+        | m <- messages
+        ]
