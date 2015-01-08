864eac35cdbc5b2508bb101c80856eaeddfd1da9
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-01-08 23:46:53 +0100

    Add empty functions to pass tests on IMAP

--- src/Network/Mail/Imap.hs
+++ src/Network/Mail/Imap.hs
@@ -3,8 +3,28 @@
 
 module Network.Mail.Imap
   ( Imap
+  , fetchAll
+  , getUID
+  , getDate
+  , getSender
+  , getSubject
   ) where
 
 import Network.Mail
 import Network.Mail.Imap.Types
+
+fetchAll :: Imap [Mail]
+fetchAll = undefined
+
+getUID :: Mail -> String
+getUID _ = undefined
+
+getDate :: Mail -> String
+getDate _ = undefined
+
+getSender :: Mail -> String
+getSender _ = undefined
+
+getSubject :: Mail -> String
+getSubject _ = undefined
 
