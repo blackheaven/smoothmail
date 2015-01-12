ae2b33c6f271f2e2ba5668de61363fe4fa942702
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-01-12 22:01:18 +0100

    Precise Imap test

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -3,8 +3,18 @@ module Network.Mail.ImapSpec (main, spec) where
 import Test.Hspec
 import Network.Mail.Imap
 
-runStubTest :: Imap a -> Expectation
-runStubTest _ = True `shouldBe` True
+runStubTest :: Imap a -> a
+runStubTest _ =  undefined
+
+prettyPrintCurrentDirectory :: Imap [String]
+prettyPrintCurrentDirectory = do
+    messages <- searchAll
+    return [    show (getUID m)
+           ++ " " ++ show (getDate m)
+           ++ " " ++ show (getSender m)
+           ++ " " ++ show (getSubject m)
+           | m <- messages
+           ]
 
 main :: IO ()
 main = hspec spec
@@ -12,11 +22,8 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "DSL usecases" $ do
-    it "Print INBOX" $ runStubTest $ do
-      messages <- searchAll
-      return [    show (getUID m)
-        ++ " " ++ show (getDate m)
-        ++ " " ++ show (getSender m)
-        ++ " " ++ show (getSubject m)
-        | m <- messages
-        ]
+    it "Print INBOX" $ do
+      runStubTest prettyPrintCurrentDirectory `shouldBe` [
+                                                           "1 2015-01-01 10:10 S1 T1"
+                                                         , "2 2015-02-03 21:12 S1 T2"
+                                                         ]
