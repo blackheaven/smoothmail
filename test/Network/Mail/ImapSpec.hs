44b5c733f0c8795cc699c301d080cd87ee4478da
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-01-28 22:54:05 +0100

    Reorganise Imap test by function

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -29,29 +29,29 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "DSL usecases" $ do
-    describe "'INBOX' directory" $ do
-      it "Print all mails" $ do
-        runStubTest prettyPrintCurrentDirectory
-        `shouldBe` Just ["1 2015-01-01 10:10 S1 T1", "2 2015-02-03 21:12 S2 T2"]
-      it "Print the first mail" $ do
-        runStubTest prettyPrintFirstMailOfCurrentDirectory
-        `shouldBe` Just ["1 2015-01-01 10:10 S1 T1"]
-      it "Print all mails after going into a folder and going up" $ do
-        runStubTest (select "Personal" >> select ".." >> prettyPrintCurrentDirectory)
-        `shouldBe` Just ["1 2015-01-01 10:10 S1 T1", "2 2015-02-03 21:12 S2 T2"]
-    describe "'Personal' directory" $ do
-      it "Print all mails" $ do
-        runStubTest (select "Personal" >> prettyPrintCurrentDirectory)
-        `shouldBe` Just ["3 2015-04-05 12:34 S2 T1"]
-    describe "Unknown directory" $ do
-      it "Print all mails" $ do
-        runStubTest (select "Unknown" >> prettyPrintCurrentDirectory)
-        `shouldBe` Nothing
-    describe "New directory" $ do
-      it "Create a non-existing directory should be true" $ do
-        runStubTest (create "New")
-        `shouldBe` True
-    describe "New directory" $ do
-      it "Create an existing directory should be false" $ do
-        runStubTest (create "Personal")
-        `shouldBe` False
+    describe "select" $ do
+      describe "'INBOX' directory" $ do
+        it "Print all mails" $ do
+          runStubTest prettyPrintCurrentDirectory
+          `shouldBe` Just ["1 2015-01-01 10:10 S1 T1", "2 2015-02-03 21:12 S2 T2"]
+        it "Print the first mail" $ do
+          runStubTest prettyPrintFirstMailOfCurrentDirectory
+          `shouldBe` Just ["1 2015-01-01 10:10 S1 T1"]
+        it "Print all mails after going into a folder and going up" $ do
+          runStubTest (select "Personal" >> select ".." >> prettyPrintCurrentDirectory)
+          `shouldBe` Just ["1 2015-01-01 10:10 S1 T1", "2 2015-02-03 21:12 S2 T2"]
+      describe "'Personal' directory" $ do
+        it "Print all mails" $ do
+          runStubTest (select "Personal" >> prettyPrintCurrentDirectory)
+          `shouldBe` Just ["3 2015-04-05 12:34 S2 T1"]
+      describe "Unknown directory" $ do
+        it "Print nothing" $ do
+          runStubTest (select "Unknown" >> prettyPrintCurrentDirectory)
+          `shouldBe` Nothing
+    describe "create" $ do
+        it "Create a non-existing directory should be true" $ do
+          runStubTest (create "New")
+          `shouldBe` True
+        it "Create an existing directory should be false" $ do
+          runStubTest (create "Personal")
+          `shouldBe` False
