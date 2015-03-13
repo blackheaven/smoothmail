9f9eedd1f72d38f99b4b896d6977ae828ef792ff
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-03-13 23:26:49 +0100

    Continue Imap's examine deeper draft

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -152,6 +152,6 @@ spec = do
         it "Examine a directory and print all mails should print those of 'INBOX'" $ do
           runStubTest (examine "Personal" >> prettyPrintCurrentDirectory)
           `shouldBe` Just ["1 2015-01-01 10:10 S1 T1", "2 2015-02-03 21:12 S2 T2"]
-        it "Examine a directory should return something" $ do
+        it "Examine a directory should return a DirectoryDescription with only one mail" $ do
           runStubTest (examine "Personal")
           `shouldBe` Just (DirectoryDescription 0 1 0)
