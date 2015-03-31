9e23c282949839c90045d1fa744bb7afaa7e5426
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-03-31 23:40:25 +0000

    Refactor Imap's stub

--- test/Network/Mail/ImapStub.hs
+++ test/Network/Mail/ImapStub.hs
@@ -29,33 +29,48 @@ debugStubTest = flip runState "/" . iterM eval
 
 eval :: ImapF (State String a) -> State String a
 eval x = case x of
-           Search _            n -> get >>= \d -> n $ fmap (M.keys) (M.lookup d mails)
-           Fetch uids FQHeader n -> get >>= \d -> n $ fmap (\m -> map getHeader $ mapMaybe (flip M.lookup m) uids) (M.lookup d mails)
-           Select p            n -> get >>= \o -> put (canonicalize $ o ++ "/" ++ p) >> get >>= \d -> n (fmap makeDirectoryDescription (M.lookup d mails))
-           Create d            n -> get >>= \o -> n . not $ isExistingDirectory (o ++ "/" ++ d)
-           Rename d            n -> get >>= \o -> n $ (isExistingDirectory o) && (not $ isExistingDirectory (o ++ "/../" ++ d))
-           Delete d            n -> get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
-           Subscribe d         n -> get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
-           Unsubscribe d       n -> get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
-           List (Left d)       n -> get >>= \o -> n $ Just $ getSubdirectories (canonicalize (o ++ "/" ++ d))
-           Lsub (Left d)       n -> get >>= \o -> n $ Just $ getSubdirectories (canonicalize (o ++ "/" ++ d))
-           Expunge             n -> get >>= \o -> n $ o == "/" -- We only allow to delete old messages if we are on the root folder
-           Check               n ->               n
-           Examine p           n -> get >>= \o -> n $ fmap makeDirectoryDescription (M.lookup (canonicalize $ o ++ "/" ++ p) mails)
-           Noop                n -> get >>= \o -> n $ fromMaybe (DirectoryDescription 0 2 0) $ fmap makeDirectoryDescription  (M.lookup o mails)
-  where canonicalize p = onNull "/" $ case p of
-                                        ('/':'/':r) -> canonicalize ('/':r)
-                                        _           -> flattenLevels p
-        flattenLevels = intercalate "/" . dropUp . splitOn '/'
-        splitOn s a = case a of
-                       [] -> []
-                       _  -> (\(p, n) -> p:splitOn s (drop 1 n)) (break (== s) a)
-        dropUp s = case s of
-                    []          -> []
-                    (_:"..":xs) -> dropUp xs
-                    (".":xs)  -> dropUp xs
-                    (x:xs)      -> x:dropUp xs
-        onNull c r = if null r then c else r
-        isExistingDirectory d = elem (canonicalize d) (M.keys mails)
-        getSubdirectories root = let rs = splitOn '/' root in map last $ filter (and . zipWith (==) rs) $ filter (\ds -> ((length rs) + 1) == (length ds))  $ map (splitOn '/') $ map fst $ M.toList mails
-        makeDirectoryDescription d = DirectoryDescription 0 (toInteger $ M.size d) 0
+           Search _            n -> onSearch n
+           Fetch uids FQHeader n -> onFetch uids n
+           Select p            n -> onSelect p n
+           Create d            n -> onCreate d n
+           Rename d            n -> onRename d n
+           Delete d            n -> onDelete d n
+           Subscribe d         n -> onSubscribe d n
+           Unsubscribe d       n -> onUnsubscribe d n
+           List (Left d)       n -> onList d n
+           Lsub (Left d)       n -> onLsub d n
+           Expunge             n -> onExpunge n
+           Check               n -> n
+           Examine p           n -> onExamine p n
+           Noop                n -> onNoop n
+
+onSearch              n = get >>= \d -> n $ fmap (M.keys) (M.lookup d mails)
+onFetch uids          n = get >>= \d -> n $ fmap (\m -> map getHeader $ mapMaybe (flip M.lookup m) uids) (M.lookup d mails)
+onSelect p            n = get >>= \o -> put (canonicalize $ o ++ "/" ++ p) >> get >>= \d -> n (fmap makeDirectoryDescription (M.lookup d mails))
+onCreate d            n = get >>= \o -> n . not $ isExistingDirectory (o ++ "/" ++ d)
+onRename d            n = get >>= \o -> n $ (isExistingDirectory o) && (not $ isExistingDirectory (o ++ "/../" ++ d))
+onDelete d            n = get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
+onSubscribe d         n = get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
+onUnsubscribe d       n = get >>= \o -> n $ isExistingDirectory (o ++ "/" ++ d)
+onList d              n = get >>= \o -> n $ Just $ getSubdirectories (canonicalize (o ++ "/" ++ d))
+onLsub d              n = get >>= \o -> n $ Just $ getSubdirectories (canonicalize (o ++ "/" ++ d))
+onExpunge             n = get >>= \o -> n $ o == "/" -- We only allow to delete old messages if we are on the root folder
+onExamine p           n = get >>= \o -> n $ fmap makeDirectoryDescription (M.lookup (canonicalize $ o ++ "/" ++ p) mails)
+onNoop                n = get >>= \o -> n $ fromMaybe (DirectoryDescription 0 2 0) $ fmap makeDirectoryDescription  (M.lookup o mails)
+
+canonicalize p = onNull "/" $ case p of
+                                ('/':'/':r) -> canonicalize ('/':r)
+                                _           -> flattenLevels p
+flattenLevels = intercalate "/" . dropUp . splitOn '/'
+splitOn s a = case a of
+               [] -> []
+               _  -> (\(p, n) -> p:splitOn s (drop 1 n)) (break (== s) a)
+dropUp s = case s of
+            []          -> []
+            (_:"..":xs) -> dropUp xs
+            (".":xs)  -> dropUp xs
+            (x:xs)      -> x:dropUp xs
+onNull c r = if null r then c else r
+isExistingDirectory d = elem (canonicalize d) (M.keys mails)
+getSubdirectories root = let rs = splitOn '/' root in map last $ filter (and . zipWith (==) rs) $ filter (\ds -> ((length rs) + 1) == (length ds))  $ map (splitOn '/') $ map fst $ M.toList mails
+makeDirectoryDescription d = DirectoryDescription 0 (toInteger $ M.size d) 0
