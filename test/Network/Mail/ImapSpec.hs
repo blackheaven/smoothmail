1024d248b812eb3ecb94da45ef271f745b2d22fb
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-04-10 00:17:07 +0000

    Add Imap's status tests on Recent

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -205,3 +205,10 @@ spec = do
         it "status on 'Personal' should return a DirectoryDescription with two mails" $ do
           runStubTest (status "Personal" [Messages])
           `shouldBe` Just (DirectoryDescription 0 1 0)
+      describe "recent item" $ do
+        it "status on 'INBOX' should return a DirectoryDescription with two mails" $ do
+          runStubTest (status "." [Recent])
+          `shouldBe` Just (DirectoryDescription 0 2 0)
+        it "status on 'Personal' should return a DirectoryDescription with two mails" $ do
+          runStubTest (status "Personal" [Recent])
+          `shouldBe` Just (DirectoryDescription 0 1 0)
