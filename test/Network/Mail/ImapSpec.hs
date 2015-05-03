e20636c152c4317fa27fb61f7a9bd9d365f09ae2
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-05-03 21:54:57 +0000

    Complete Imap's status all itemsspec

--- test/Network/Mail/ImapSpec.hs
+++ test/Network/Mail/ImapSpec.hs
@@ -229,7 +229,7 @@ spec = do
       describe "all items" $ do
         it "status on 'INBOX' should return with no mail" $ do
           runStubTest (status "." (SQProduct (SQProduct SQUnseen SQMessages) (SQProduct SQRecent (SQProduct SQUidnext SQUidvalidity))))
-          `shouldBe` Just undefined
+          `shouldBe` Just ((2, 2), (2, (UID 1, UID 2)))
         it "status on 'Personal' should return with no mail" $ do
           runStubTest (status "Personal" (SQProduct (SQProduct SQUnseen SQMessages) (SQProduct SQRecent (SQProduct SQUidnext SQUidvalidity))))
           `shouldBe` Just undefined
