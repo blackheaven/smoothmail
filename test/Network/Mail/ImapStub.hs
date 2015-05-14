a4667db6bdee34f5ddfc62ab2ace73399dc504a4
Gautier DI FOLCO <gautier.difolco@gmail.com>
2015-05-14 22:56:29 +0000

    Correct Imap's status Uidnext spec

--- test/Network/Mail/ImapStub.hs
+++ test/Network/Mail/ImapStub.hs
@@ -125,7 +125,7 @@ onStatus d i n = do
                           SQProduct a b -> (extractInfo a m, extractInfo b m)
                           SQMessages    -> 2
                           SQRecent      -> 2
-                          SQUidnext     -> UID $ extractUID $ maximum $ map fst $ concatMap (M.toList . snd) $ M.toList mails
+                          SQUidnext     -> UID $ (1+) . extractUID $ maximum $ map fst $ concatMap (M.toList . snd) $ M.toList mails
                           SQUidvalidity -> UID $ extractUID $ maximum $ map fst $ M.toList m
                           SQUnseen      -> 2
 
